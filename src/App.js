b3ac3f1b5357e35f9a728ff839f9b42fb446f13d
nattawat <nattawatx86@gmail.com>
2019-09-22 00:35:38 +0700

    scan ได้

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	state = {
+		bluetoothDevice: null,
+		server: null
+	}
+
+	handleScanButton = async e => {
+		e.preventDefault()
+		await this.setState({
+			bluetoothDevice: navigator.bluetooth.requestDevice({
+				acceptAllDevices: ["true"],
+				optionalServices: ["4fafc201-1fb5-459e-8fcc-c5c9c331914b"]
+			})
+		})
+	}
+
+	connectToDevice = () => {
+		this.setState({
+			server: this.state.gatt.connect()
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<button onClick={this.handleScanButton}>scan</button>
+				<br />
+				<br />
+				<form>
+					<input id='deauth' type='text' placeholder='deauth' />
+					<input id='beacon' type='text' placeholder='beacon' />
+					<input id='probe' type='text' placeholder='probe' />
+					<button>send value</button>
+				</form>
+
+				<button type='button' onclick='a()'>
+					training
+				</button>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
